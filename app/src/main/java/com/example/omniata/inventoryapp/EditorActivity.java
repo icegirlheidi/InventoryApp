64afc15dbfeaf00e984b121192b06dc005d3a0f6
Yi Wang <icegirlheidi@gmail.com>
2017-06-20 10:07:20 +0300

    Add layout for editor

--- app/src/main/java/com/example/omniata/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/omniata/inventoryapp/EditorActivity.java
@@ -1,9 +1,30 @@
 package com.example.omniata.inventoryapp;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditorActivity extends AppCompatActivity {
+
+    private EditText mNameTextView;
+    private EditText mSupplierTextView;
+    private EditText mUnitPriceTextView;
+    private EditText mQuantityTextView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editor_layout);
+
+        mNameTextView = (EditText) findViewById(R.id.product_name);
+        mSupplierTextView = (EditText) findViewById(R.id.product_supplier);
+        mUnitPriceTextView = (EditText) findViewById(R.id.product_unit_price);
+        mQuantityTextView = (EditText) findViewById(R.id.product_quantity);
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
