a11ed2ca162c93f456907afc249417364a5d1d29
Yi Wang <icegirlheidi@gmail.com>
2017-06-22 10:52:02 +0300

    Add newView and bindView methods in ProductCursorAdapter

--- app/src/main/java/com/example/omniata/inventoryapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/omniata/inventoryapp/ProductCursorAdapter.java
@@ -3,9 +3,15 @@ package com.example.omniata.inventoryapp;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.omniata.inventoryapp.data.ProductContract.ProductEntry;
+
+import org.w3c.dom.Text;
 
 public class ProductCursorAdapter extends CursorAdapter {
     public ProductCursorAdapter(Context context, Cursor c) {
@@ -14,11 +20,24 @@ public class ProductCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+
+        TextView nameTextView = (TextView) view.findViewById(R.id.list_item_name);
+        TextView priceTextView = (TextView) view.findViewById(R.id.list_item_unit_price);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.list_item_quantity);
+
+        String name = cursor.getString(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME));
+        String price = cursor.getString(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE));
+        String quantity = cursor.getString(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY));
+
+        nameTextView.setText(name);
+        priceTextView.setText(price);
+        quantityTextView.setText(quantity);
+
 
     }
 }
