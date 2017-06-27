b2293e2e190b54a6f639b5d9c7812bc908165571
Yi Wang <icegirlheidi@gmail.com>
2017-06-27 13:55:59 +0300

    Refactor ProductCursorAdapter to decrease product quantity

--- app/src/main/java/com/example/omniata/inventoryapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/omniata/inventoryapp/ProductCursorAdapter.java
@@ -1,43 +1,84 @@
 package com.example.omniata.inventoryapp;
 
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.omniata.inventoryapp.data.ProductContract.ProductEntry;
 
 import org.w3c.dom.Text;
 
 public class ProductCursorAdapter extends CursorAdapter {
+
+    static class Holder {
+        TextView mNameTextView;
+        TextView mPriceTextView;
+        TextView mQuantityTextView;
+        ImageButton mSaleButton;
+
+        public Holder(View view) {
+            mNameTextView = (TextView) view.findViewById(R.id.list_item_name);
+            mPriceTextView = (TextView) view.findViewById(R.id.list_item_unit_price);
+            mQuantityTextView = (TextView) view.findViewById(R.id.list_item_quantity);
+            mSaleButton = (ImageButton) view.findViewById(R.id.sale_button);
+        }
+    }
+
     public ProductCursorAdapter(Context context, Cursor c) {
         super(context, c);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+        Holder holder = new Holder(view);
+        view.setTag(holder);
+        return view;
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, Context context, final Cursor cursor) {
 
-        TextView nameTextView = (TextView) view.findViewById(R.id.list_item_name);
-        TextView priceTextView = (TextView) view.findViewById(R.id.list_item_unit_price);
-        TextView quantityTextView = (TextView) view.findViewById(R.id.list_item_quantity);
+        final Holder holder = (Holder) view.getTag();
 
         String name = cursor.getString(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME));
-        String price = cursor.getString(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE));
-        String quantity = cursor.getString(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY));
+        Double price = cursor.getDouble(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE));
+        int quantity = cursor.getInt(cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_QUANTITY));
 
-        nameTextView.setText(name);
-        priceTextView.setText(price);
-        quantityTextView.setText(quantity);
+        holder.mNameTextView.setText(name);
+        holder.mPriceTextView.setText(String.valueOf(price));
+        holder.mQuantityTextView.setText(String.valueOf(quantity));
 
+        holder.mSaleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentQuantity = Integer.parseInt(holder.mQuantityTextView.getText().toString().trim());
+                currentQuantity--;
+                holder.mQuantityTextView.setText(String.valueOf(currentQuantity));
 
+                ContentValues values = new ContentValues();
+                values.put(ProductEntry.COLUMN_PRODUCT_QUANTITY, currentQuantity);
+
+                String selection = ProductEntry._ID + "=?";
+                long id = cursor.getLong(cursor.getColumnIndex(ProductEntry._ID));
+                String[] selectionArgs = new String[]{String.valueOf(id)};
+                Toast.makeText(view.getContext(), "Clicked item position: " + cursor.getPosition(), Toast.LENGTH_SHORT);
+                view.getContext().getContentResolver().update(
+                        ProductEntry.CONTENT_URI,
+                        values,
+                        selection,
+                        selectionArgs);
+            }
+        });
     }
 }
